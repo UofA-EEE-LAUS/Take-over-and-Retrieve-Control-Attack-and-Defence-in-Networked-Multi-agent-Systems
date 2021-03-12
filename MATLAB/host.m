8ea65d842c274a05a23b88bbf0bef4fa4c360028
star13146 <61785607+star13146@users.noreply.github.com>
2021-03-12 14:26:56 +0800

    a new test demo agent_test_v1

--- MATLAB/host.m
+++ MATLAB/host.m
@@ -1,30 +1,121 @@
-function state=hostsent(hostport,input)%Ô­À´½Ðsent
-%add a counter to input
+% FYP 7331
+% host.m
+% By Liuxin Shen
+% This class defines a host class with its properties and methods
 
+% Update 2021.3.9
+% By Zhiang Chen
+% Complete method hostsent, feedback and resend.
+% Complete method hostread 
+% This class is used to represent a host in the MAS control
+classdef host <handle
+    properties 
+        u; %host udp varaible
+        counter; %the unique ID of this host
+        history; %stores the historial message sent from the host
+        target_ports; %Store the id of each rover and their corresponding port number
+    end
+    methods
+        
+        % Constuctor takes the locaport and remote port as input
+        function obj= host(localport,remoteport)
+        obj.u = udp('127.0.0.1','RemotePort',remoteport,'LocalPort',localport);
+        fopen (obj.u);
+        obj.counter=0;
+        obj.history=[];
+        obj.target_ports=[remoteport+1 localport 1;remoteport+2 localport 2;remoteport+3 localport 3];
+        end
+        
+        % Change remote port of the host
+        function state= setRemotePort(obj,new_remoteport)
+        state=1;
+        obj.u.Remoteport=new_remoteport;
+        end
 
-%store the msg in a variable called history
+ 
+        function state=hostsent(obj,rover_id,input)%change remote_port to rover iD
+        %add a counter to input
+        state=1;
+        msg_c=num2str(obj.counter+rover_id*10);
+        msg=char(msg_c+input);
+        obj.counter=obj.counter+1;
+        %store the msg in msg history
+        obj.history=[obj.history;msg];
+        %send msg to agent, and report if successful
+        disp('Message sent');
+        agent_port=obj.target_ports(rover_id,1);        
+        writeUDP(obj,agent_port,msg); %use udp write function instead of fwrite
+        end
+        
+        %read from the local port and delete the relate history msg
+        function state=feedback(obj,msgcounter)
+            state=1;
+            %delete the related msg stored in history
+            [m,~]=size(obj.history);
+            disp(m);
+            ID=str2num(msgcounter);
+            for i=1:m
+                if (obj.history(i,1)==msgcounter)
+                obj.history(i,:)=[];
+                %print some information here
+                fprintf("message %d Successfully deleted",ID);
+                break;
+                elseif i==m
+                    disp('Information not included');
+                end
+            end
+        end
+        
+        %accept a reset order, update the new target_ports
+        function state=resent(obj,roverID)
+            state=1;
+        %based on input msgcounter,find the relate msg in history
+        [m,~]=size(obj.history);
+        msg=[];
+        target_port=obj.target_ports(roverID,1);
+            for i=1:m
+                if (str2num(obj.history(i,1))==roverID)
+                    msg=obj.history(i,:);
+                    break;
+                end
+            end
+            %send the msg to the new agent_port
+            obj.writeUDP(target_port,msg);
+        end
+        
+        function state=writeUDP(obj,targetport,msg)
+            state=1;
+            obj.setRemotePort(targetport);
+            fwrite(obj.u,msg);
+        end
+        
+        
+        function state=hostread(obj)
+           state=1;
+           msg = fread(obj.u);
+           if(msg(1)~='-')
+               feedback(obj,msg);
+           else
+               %update the new port number to the host
+               roverid=str2num(msg(2));
+               new_targetport=str2num(msg(3:6));
+               msgID=str2num(msg(7:8));
+               if 0<roverid<4
+                   obj.targetports(roverid,1)=new_targetport;
+                   if (resent(obj,roverid)==1)
+                       disp('resent successful');
+                   else 
+                       disp('resent failed');
+                   end
+               else 
+                   disp('error,out of boundary');
+               end
+           end
+        end
 
-
-%send msg to agent, and report if successful
-state=fwrite(hostport,msg);
-
-
-end
-
-function state=feedback(msgcounter)
-%delete the related msg stored in history
-
-end
-function state=resent(msgID,agent_port)
-%based on input msgcounter,find the relate msg in history
-
-
-%send the msg to the new agent_port
-end
-
-function state=udpwrite(targetport,msg)
-%used to write a msg to a target udp port
-fwrite(targetport,msg);
-disp('message sent successful');
-state=1;
+        function delete(obj)
+            fclose(obj.u);
+            delete(obj.u);
+        end
+    end
 end
--- MATLAB/rover.m
+++ MATLAB/rover.m
@@ -3,7 +3,17 @@
 % By Moyang Feng
 % This class defines a rover object and its useful methods
 
-classdef rover
+%v0.2 update 2021.3.8 
+%By Liuxin Shen
+%Improve the UDPwrite function to transfer feedback to the host
+%add variable msgID in UDP receive data to the current class.
+%update writeUDP function
+
+% v0.3 update 2021.3.11
+% By Liuxin Shen 
+% Update rover feedback function
+% Update feedback and receive function
+classdef rover <handle
     
     properties
         % basic properties
@@ -25,7 +35,8 @@ classdef rover
         % UDP host and received data
         u;
         port;
-        received;
+        msgID;
+        flag;
     end
     
     methods
@@ -51,6 +62,8 @@ classdef rover
             obj.u = udp('127.0.0.1','RemotePort',4012,'LocalPort',obj.port);
             obj.u.EnablePortSharing = 'on';
             fopen(obj.u);
+            obj.flag=0;
+            obj.msgID=10*roverID;
         end
         
         % destructor - IMPORTANT: Clean up UDP ports
@@ -61,16 +74,21 @@ classdef rover
         end
         
         % methods
-        
+        function state=feedback(obj,msgID)
+            state=1;
+            obj.writeUDP(msgID);
+        end
         % feedback rover status to the host
-        function writeUDP(obj,host)
-            data = [obj.roverID obj.availability obj.leadership];
-            fwrite(host,data);
+        function writeUDP(obj,msg)
+            
+            fwrite(obj.u,msg);
         end
         
         % read received data from host
-        function received = readUDP(obj,n)
-            received = fread(obj.u,n);
+        function received = readUDP(obj)
+            %detection update later
+            received = fread(obj.u);
+%             obj.receivelist= [obj.receivelist received(1:2);
         end
         
         % get the distance and angle from current position to target
@@ -78,6 +96,22 @@ classdef rover
             distance = norm(obj.target(1:2) - obj.position(1:2));
             angle = abs((obj.target(3) - rad2deg(obj.orientation(3)) - 150));
             diff = [distance angle];
+        end
+        
+        function state=reset_port(obj)
+            state=1;
+%             obj.flag=6;
+            %reset the port
+            fclose(obj.u);
+            delete(obj.u);
+            obj.port = obj.port+10;
+            disp(obj.port);
+            obj.u = udp('127.0.0.1','RemotePort',4012,'LocalPort',obj.port);
+            fopen(obj.u);
+            %sent the reset ID to the agent 
+            reset_msg=['-' int2str(obj.roverID) int2str(obj.port) int2str(obj.msgID)];
+            obj.writeUDP(reset_msg);
+            
         end
         
         % print basic info of the rover
